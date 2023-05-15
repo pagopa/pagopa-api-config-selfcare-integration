07725bf49d0d9b3150a951d5aa44c7be600eb31c
Andrea De Rinaldis <117269497+andrea-deri@users.noreply.github.com>
2023-05-15 13:22:02 +0200

    [PAGOPA-954] Removed code smells

--- src/main/java/it/gov/pagopa/apiconfig/selfcareintegration/controller/BrokerController.java
+++ src/main/java/it/gov/pagopa/apiconfig/selfcareintegration/controller/BrokerController.java
@@ -24,8 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
 @RestController()
