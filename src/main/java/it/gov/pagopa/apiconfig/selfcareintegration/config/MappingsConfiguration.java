3cd9983c1d589c6034ecee47ab1e4c1426019502
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-05-22 15:19:15 +0200

    [PAGOPA-931] feat: added dedicated mapper for new API

--- src/main/java/it/gov/pagopa/apiconfig/selfcareintegration/config/MappingsConfiguration.java
+++ src/main/java/it/gov/pagopa/apiconfig/selfcareintegration/config/MappingsConfiguration.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.apiconfig.selfcareintegration.config;
 
-
+import it.gov.pagopa.apiconfig.selfcareintegration.mapper.ConvertCanaliToChannelDetails;
+import it.gov.pagopa.apiconfig.selfcareintegration.model.channel.ChannelDetails;
+import it.gov.pagopa.apiconfig.starter.entity.Canali;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +15,12 @@ public class MappingsConfiguration {
   ModelMapper modelMapper() {
     ModelMapper mapper = new ModelMapper();
     mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+    // insert here the new mappers
+    mapper
+        .createTypeMap(Canali.class, ChannelDetails.class)
+        .setConverter(new ConvertCanaliToChannelDetails());
+
     return mapper;
   }
-
 }
