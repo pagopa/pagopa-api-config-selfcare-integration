a6f394112db153303de4f921d04eb87d0ea2b56d
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-05-23 15:33:54 +0200

    [PAGOPA-931] feat: updated integration tests

--- integration-test/src/step_definitions/support/logic/common_logic.js
+++ integration-test/src/step_definitions/support/logic/common_logic.js
@@ -1,22 +1,22 @@
 const assert = require("assert");
 
 async function assertEmptyList(response) {
-    console.log(` - -> the client receives an empty list..`);
+    console.log(` - Then the client receives an empty list..`);
     assert.ok(response.length === 0);
 }
 
 async function assertErrorMessage(response) {
-    console.log(` - -> the client receives an error message..`);
+    console.log(` - Then the client receives an error message..`);
     assert.ok(response !== null && response !== undefined);
 }
 
 async function assertNonEmptyList(response) {
-    console.log(` - -> the client receives a non-empty list..`);
+    console.log(` - Then the client receives a non-empty list..`);
     assert.ok(response.length > 0);
 }
 
 async function assertStatusCode(response, statusCode) {
-    console.log(` - -> the client receives status code [${statusCode}]..`);
+    console.log(` - Then the client receives status code [${statusCode}]..`);
     assert.strictEqual(response.status, statusCode);
 }
 
--- integration-test/src/step_definitions/support/steps.js
+++ integration-test/src/step_definitions/support/steps.js
@@ -7,27 +7,36 @@ const {
 } = require('./logic/common_logic');
 const { executeHealthCheckForAPIConfig } = require('./logic/health_checks_logic');
 const {
+  assertChannelIncludedInResponse,
   assertStationIncludedInResponse,
-
   retrieveBroker,
   retrieveBrokerWithNoStation,
   retrieveNonExistingBroker,
   retrieveCreditorInstitution,
   retrieveCreditorInstitutionWithNoStation,
   retrieveNonExistingCreditorInstitution,
+  retrievePSPBroker,
+  retrievePSPBrokerWithNoChannel,
+  retrieveNonExistingPSPBroker,
   retrieveStationsByBroker,
   retrieveStationsByCreditorInstitution,
+  retrieveChannelsByPSPBroker,
   retrieveStationRelatedToBroker,
-  retrieveStationRelatedToCI
+  retrieveStationRelatedToCI,
+  retrieveChannelRelatedToPSPBroker
 } = require('./logic/selfcare_integration_logic');
 
 let bundle = {
   brokerId: undefined,
   creditorInstitutionId: undefined,
   stationId: undefined,
+  pspBrokerId: undefined,
+  channelId: undefined,
   broker: undefined,
   creditorInstitution: undefined,
   station: undefined,
+  pspBroker: undefined,
+  channel: undefined,
   response: undefined,
   limit: 100,
   pageNumber: 0
@@ -47,23 +56,31 @@ Given('a non-existing broker', () => retrieveNonExistingBroker(bundle));
 Given('an existing creditor institution', () => retrieveCreditorInstitution(bundle));
 Given('an existing creditor institution with no stations related', () => retrieveCreditorInstitutionWithNoStation(bundle));
 Given('a non-existing creditor institution', () => retrieveNonExistingCreditorInstitution(bundle));
+Given('an existing PSP broker', () => retrievePSPBroker(bundle));
+Given('an existing PSP broker with no channels related', () => retrievePSPBrokerWithNoChannel(bundle));
+Given('a non-existing PSP broker', () => retrieveNonExistingPSPBroker(bundle));
 Given('a station related to broker', () => retrieveStationRelatedToBroker(bundle));
 Given('a station related to creditor institution', () => retrieveStationRelatedToCI(bundle));
+Given('a channel related to PSP broker', () => retrieveChannelRelatedToPSPBroker(bundle));
 
 /*
  *  'When' clauses for executing actions.
  */
 When('the client requests the list of stations related to the broker', () => retrieveStationsByBroker(bundle));
 When('the client requests the list of stations related to the creditor institution', () => retrieveStationsByCreditorInstitution(bundle));
+When('the client requests the list of channels related to the PSP broker', () => retrieveChannelsByPSPBroker(bundle));
 
 /*
  *  'Then' clauses for assering retrieved data
  */
 Then('the client receives status code {int}', (statusCode) => assertStatusCode(bundle.response, statusCode));
 Then('the client receives a non-empty list of stations', () => assertNonEmptyList(bundle.response?.data.stations));
+Then('the client receives a non-empty list of channels', () => assertNonEmptyList(bundle.response?.data.channels));
 Then('the client receives an empty list of stations', () => assertEmptyList(bundle.response?.data?.stations));
+Then('the client receives an empty list of channels', () => assertEmptyList(bundle.response?.data?.channels));
 Then('the client receives an error message', () => assertErrorMessage(bundle.response?.data));
 Then('the station is included in the result list', () => assertStationIncludedInResponse(bundle.stationId, bundle.response));
+Then('the channel is included in the result list', () => assertChannelIncludedInResponse(bundle.channelId, bundle.response));
 
 
 Before(function(scenario) {
