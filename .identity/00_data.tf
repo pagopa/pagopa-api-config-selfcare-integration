00efc3f76198bf8eeffc1a41c680bc814c074a55
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-05-26 16:48:07 +0200

    fix keyvault data

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -13,28 +13,28 @@ data "github_organization_teams" "all" {
 }
 
 data "azurerm_key_vault" "key_vault" {
-  count  = var.env_short == "d" ? 1 : 0
+  count  = var.env_short != "p" ? 1 : 0
 
   name = "pagopa-${var.env_short}-kv"
   resource_group_name = "pagopa-${var.env_short}-sec-rg"
 }
 
 data "azurerm_key_vault_secret" "key_vault_sonar" {
-  count  = var.env_short == "d" ? 1 : 0
+  count  = var.env_short != "p" ? 1 : 0
 
   name = "sonar-token"
   key_vault_id = data.azurerm_key_vault.key_vault[0].id
 }
 
 data "azurerm_key_vault_secret" "key_vault_bot_token" {
-  count  = var.env_short == "d" ? 1 : 0
+  count  = var.env_short != "p" ? 1 : 0
 
   name = "bot-token-github"
   key_vault_id = data.azurerm_key_vault.key_vault[0].id
 }
 
 data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
-  count  = var.env_short == "d" ? 1 : 0
+  count  = var.env_short != "p" ? 1 : 0
 
   name = "cucumber-token"
   key_vault_id = data.azurerm_key_vault.key_vault[0].id
