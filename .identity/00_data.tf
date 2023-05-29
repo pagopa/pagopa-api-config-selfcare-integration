09e1807f30235ebdb53bb95de1d8a363a1300ae6
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-05-29 12:41:00 +0200

    [PAGOPA-961] fix: updated secret reference

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -13,27 +13,25 @@ data "github_organization_teams" "all" {
 }
 
 data "azurerm_key_vault" "key_vault" {
+
   name = "pagopa-${var.env_short}-kv"
   resource_group_name = "pagopa-${var.env_short}-sec-rg"
 }
 
 data "azurerm_key_vault_secret" "key_vault_sonar" {
-  count  = var.env_short != "d" ? 1 : 0
 
   name = "sonar-token"
-  key_vault_id = data.azurerm_key_vault.key_vault[0].id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
 data "azurerm_key_vault_secret" "key_vault_bot_token" {
-  count  = var.env_short != "d" ? 1 : 0
 
   name = "bot-token-github"
-  key_vault_id = data.azurerm_key_vault.key_vault[0].id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
 data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
-  count  = var.env_short != "d" ? 1 : 0
 
   name = "cucumber-token"
-  key_vault_id = data.azurerm_key_vault.key_vault[0].id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
