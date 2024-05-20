a0a40f5204e9cc9d63bb860338a38bcf249247da
giomella <giomella@emeal.nttdata.com>
2024-05-20 16:47:56 +0200

    [VAS-912] updated openapi

--- src/main/java/it/gov/pagopa/apiconfig/selfcareintegration/controller/CreditorInstitutionController.java
+++ src/main/java/it/gov/pagopa/apiconfig/selfcareintegration/controller/CreditorInstitutionController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
@@ -48,176 +47,95 @@ public class CreditorInstitutionController {
     }
 
     /**
-   * GET /{creditorInstitutionCode}/stationsdetails : Get creditor institution station
-   *
-   * @param creditorInstitutionCode station code. (required)
-   * @return OK. (status code 200) or Not Found (status code 404) or Service unavailable (status
-   *     code 500)
-   */
-  @Operation(
-      summary = "Get creditor institution station list",
-      security = {
-        @SecurityRequirement(name = "ApiKey"),
-        @SecurityRequirement(name = "Authorization")
-      },
-      tags = {
-        "Creditor Institutions",
-      })
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "OK",
-            content =
-                @Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = StationDetailsList.class))),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Unauthorized",
-            content = @Content(schema = @Schema())),
-        @ApiResponse(
-            responseCode = "403",
-            description = "Forbidden",
-            content = @Content(schema = @Schema())),
-        @ApiResponse(
-            responseCode = "404",
-            description = "Not Found",
-            content = @Content(schema = @Schema(implementation = ProblemJson.class))),
-        @ApiResponse(
-            responseCode = "429",
-            description = "Too many requests",
-            content = @Content(schema = @Schema())),
-        @ApiResponse(
-            responseCode = "500",
-            description = "Service unavailable",
-            content =
-                @Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ProblemJson.class)))
-      })
-  @GetMapping(
-      value = "/{creditorInstitutionCode}/stations",
-      produces = {MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<StationDetailsList> getStationsDetailsFromCreditorInstitution(
-      @Parameter(
-              description = "Organization fiscal code, the fiscal code of the Organization.",
-              required = true)
-          @PathVariable("creditorInstitutionCode")
-          String creditorInstitutionCode,
-      @Valid
-          @Parameter(
-              description = "The number of elements to be included in the page.",
-              required = true)
-          @RequestParam(required = false, defaultValue = "10")
-          @Positive
-          @Max(999)
-          Integer limit,
-      @Valid
-          @Parameter(description = "The index of the page, starting from 0.", required = true)
-          @Min(0)
-          @RequestParam(required = false, defaultValue = "0")
-          Integer page) {
-    return ResponseEntity.ok(
-        creditorInstitutionsService.getStationsDetailsFromCreditorInstitution(
-            creditorInstitutionCode, PageRequest.of(page, limit)));
-  }
+     * GET /{creditorInstitutionCode}/stationsdetails : Get creditor institution station
+     *
+     * @param creditorInstitutionCode station code. (required)
+     * @return OK. (status code 200) or Not Found (status code 404) or Service unavailable (status
+     * code 500)
+     */
+    @Operation(
+            summary = "Get creditor institution station list",
+            security = {@SecurityRequirement(name = "ApiKey"), @SecurityRequirement(name = "Authorization")})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = StationDetailsList.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable", content =
+            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))
+    })
+    @GetMapping(value = "/{creditorInstitutionCode}/stations", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<StationDetailsList> getStationsDetailsFromCreditorInstitution(
+            @Parameter(description = "Organization fiscal code, the fiscal code of the Organization.") @PathVariable("creditorInstitutionCode") String creditorInstitutionCode,
+            @Parameter(description = "The number of elements to be included in the page.", required = true) @RequestParam(required = false, defaultValue = "10") @Positive @Max(999) Integer limit,
+            @Parameter(description = "The index of the page, starting from 0.", required = true) @Min(0) @RequestParam(required = false, defaultValue = "0") Integer page
+    ) {
+        return ResponseEntity.ok(
+                this.creditorInstitutionsService
+                        .getStationsDetailsFromCreditorInstitution(creditorInstitutionCode, PageRequest.of(page, limit))
+        );
+    }
 
-  /**
-   * GET /{creditorInstitutionCode}/applicationcodes : Get creditor institution station
-   *
-   * @param creditorInstitutionCode station code. (required)
-   * @param showUsedCodes The flag that permits to show the codes already used
-   * @return OK. (status code 200) or Not Found (status code 404) or Service unavailable (status
-   *     code 500)
-   */
-  @Operation(
-      summary = "Get application code associations with creditor institution",
-      security = {
-        @SecurityRequirement(name = "ApiKey"),
-        @SecurityRequirement(name = "Authorization")
-      },
-      tags = {
-        "Creditor Institutions",
-      })
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "OK",
-            content =
-                @Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = CIAssociatedCodeList.class))),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Unauthorized",
-            content = @Content(schema = @Schema())),
-        @ApiResponse(
-            responseCode = "403",
-            description = "Forbidden",
-            content = @Content(schema = @Schema())),
-        @ApiResponse(
-            responseCode = "404",
-            description = "Not Found",
-            content = @Content(schema = @Schema(implementation = ProblemJson.class))),
-        @ApiResponse(
-            responseCode = "429",
-            description = "Too many requests",
-            content = @Content(schema = @Schema())),
-        @ApiResponse(
-            responseCode = "500",
-            description = "Service unavailable",
-            content =
-                @Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ProblemJson.class)))
-      })
-  @GetMapping(
-      value = "/{creditorInstitutionCode}/applicationcodes",
-      produces = {MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<CIAssociatedCodeList> getApplicationCodesFromCreditorInstitution(
-      @Parameter(
-              description = "Organization fiscal code, the fiscal code of the Organization.",
-              required = true)
-          @PathVariable("creditorInstitutionCode")
-          String creditorInstitutionCode,
-      @Parameter(
-              description = "The flag that permits to show the codes already used. Default: true")
-          @RequestParam(required = false, defaultValue = "true")
-          boolean showUsedCodes) {
-    return ResponseEntity.ok(
-        creditorInstitutionsService.getApplicationCodesFromCreditorInstitution(
-            creditorInstitutionCode, showUsedCodes));
-  }
+    /**
+     * GET /{creditorInstitutionCode}/applicationcodes : Get creditor institution station
+     *
+     * @param creditorInstitutionCode station code. (required)
+     * @param showUsedCodes           The flag that permits to show the codes already used
+     * @return OK. (status code 200) or Not Found (status code 404) or Service unavailable (status
+     * code 500)
+     */
+    @Operation(
+            summary = "Get application code associations with creditor institution",
+            security = {@SecurityRequirement(name = "ApiKey"), @SecurityRequirement(name = "Authorization")})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CIAssociatedCodeList.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))
+    })
+    @GetMapping(value = "/{creditorInstitutionCode}/applicationcodes", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<CIAssociatedCodeList> getApplicationCodesFromCreditorInstitution(
+            @Parameter(description = "Organization fiscal code, the fiscal code of the Organization.") @PathVariable("creditorInstitutionCode") String creditorInstitutionCode,
+            @Parameter(description = "The flag that permits to show the codes already used. Default: true") @RequestParam(required = false, defaultValue = "true") boolean showUsedCodes
+    ) {
+        return ResponseEntity.ok(
+                creditorInstitutionsService
+                        .getApplicationCodesFromCreditorInstitution(creditorInstitutionCode, showUsedCodes)
+        );
+    }
 
-  /**
-   * GET /{creditorInstitutionCode}/segregationcodes : Get creditor institution segregation codes
-   *
-   * @param creditorInstitutionCode station code. (required)
-   * @return OK. (status code 200) or Not Found (status code 404) or Service unavailable (status
-   *     code 500)
-   */
-  @Operation(
-      summary = "Get segregation code associations with creditor institution",
-      security = {@SecurityRequirement(name = "ApiKey"), @SecurityRequirement(name = "Authorization")},
-      tags = {"Creditor Institutions",})
-  @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "OK",
-                  content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AvailableCodes.class))),
-          @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
-          @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema())),
-          @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
-          @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
-          @ApiResponse(responseCode = "500", description = "Service unavailable",
-                  content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))
-  })
-  @GetMapping(value = "/{creditorInstitutionCode}/segregationcodes", produces = {MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<AvailableCodes> getSegregationCodesFromCreditorInstitution(
-      @Parameter(description = "Organization fiscal code, the fiscal code of the Organization.") @PathVariable String creditorInstitutionCode
-  ) {
-    return ResponseEntity.ok(this.creditorInstitutionsService.getAvailableCISegregationCodes(creditorInstitutionCode));
-  }
+    /**
+     * GET /{creditorInstitutionCode}/segregationcodes : Get creditor institution segregation codes
+     *
+     * @param creditorInstitutionCode station code. (required)
+     * @return OK. (status code 200) or Not Found (status code 404) or Service unavailable (status
+     * code 500)
+     */
+    @Operation(
+            summary = "Get segregation code associations with creditor institution",
+            security = {@SecurityRequirement(name = "ApiKey"), @SecurityRequirement(name = "Authorization")})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AvailableCodes.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))
+    })
+    @GetMapping(value = "/{creditorInstitutionCode}/segregationcodes", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<AvailableCodes> getSegregationCodesFromCreditorInstitution(
+            @Parameter(description = "Organization fiscal code, the fiscal code of the Organization.") @PathVariable String creditorInstitutionCode
+    ) {
+        return ResponseEntity.ok(this.creditorInstitutionsService.getAvailableCISegregationCodes(creditorInstitutionCode));
+    }
 
     /**
      * Retrieve a list of creditor institution business names, given the provided list of creditor institution tax codes
